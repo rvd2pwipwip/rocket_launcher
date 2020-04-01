610c8f7a340adeb3154dd70120e8383b591fb4ed
Herve Desrosiers <hdesrosiers@stingray.com>
2020-04-01 07:49:29 -0400

    Finished Accelerate the Launch

--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/AccelerateRocketAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/AccelerateRocketAnimationActivity.kt
@@ -1,6 +1,25 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateInterpolator
+
 class AccelerateRocketAnimationActivity : BaseAnimationActivity() {
   override fun onStartAnimation() {
+
+    // 1
+    val valueAnimator = ValueAnimator.ofFloat(0f, -screenHeight)
+    valueAnimator.addUpdateListener {
+      val value = it.animatedValue as Float
+      rocket.translationY = value
+    }
+
+// 2 - Here set your favorite interpolator
+    valueAnimator.interpolator = AccelerateInterpolator(3f)
+    valueAnimator.duration = DEFAULT_ANIMATION_DURATION
+
+// 3
+    valueAnimator.start()
+
+
   }
 }
